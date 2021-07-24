45f3ffeea8099c1f5b0b9af4bf8cc4511b0ae2f8
sempfa <sempfa@amazon.com>
2021-07-24 17:42:26 +1000

    add frist code

---
+++ main.py
@@ -0,0 +1,62 @@
+import random, sys
+
+Options = ["Rock", "Paper", "Scissors", "Lizzard", "Spock"]
+
+def getComputerChoice():
+    return random.choice(Options)
+
+def getPlayerChoice():
+    return Options[int(input('Enter the number of your choice: '))-1]
+
+def printOptions():
+    print('\n'.join(f'({i+1}) {option.title()}' for i, option in enumerate(Options)))
+
+def printChoices(playerChoice, computerChoice):
+    print(f'You chose {playerChoice}')
+    print(f'The computer chose {computerChoice}')
+
+def printOutcome(playerChoice, computerChoice, choiceBeats, choiceLoseTo):
+    if computerChoice in choiceLoseTo:
+        print(f'Sorry {computerChoice} beats {playerChoice}' )
+    elif computerChoice in choiceBeats:
+        print(f'Yes! {playerChoice} beats {computerChoice}')
+    print('\n')
+
+def printResult(playerChoice, computerChoice):
+    if playerChoice == computerChoice:
+        print("Draw!")
+
+    if playerChoice == 'Rock':
+        printOutcome('Rock', computerChoice, ['Scissors', 'Lizzard'], ['Papar', 'Spock'])
+    elif playerChoice == "Paper":
+        printOutcome('Paper', computerChoice, ['Rock', 'Spock'], ['Scissors', 'Lizzard'])
+    elif playerChoice == "Scissors":
+        printOutcome('Scissors', computerChoice, ['Papar', 'Lizzard'], ['Spock', 'Rock'])
+    elif playerChoice == "Lizzard":
+        printOutcome('Lizzard', computerChoice, ['Spock', 'Papar'], ['Scissors', 'Rock'])
+    elif playerChoice == "Spock":
+        printOutcome('Spock', computerChoice, ['Scissors', 'Rock'], ['Lizzard', 'Papar'])
+
+def startNewGame():
+    option = input('Start new game Y/n: ')
+    if option == 'n':
+        return 'n'
+    else: 
+        return 'y'
+
+def main():
+    run = True
+    while run:
+        printOptions()
+        playerChoice = getPlayerChoice()
+        computerChoice = getComputerChoice()
+
+        printChoices(playerChoice,computerChoice)
+        printResult(playerChoice, computerChoice)
+        playerChoice = startNewGame()
+
+        if playerChoice == "n":
+            run = False
+            sys.exit()
+
+main()
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Rock Paper Scissors 
+The Bing Bang edition
+
+A simple demonstration of a game loop in python.
+
+## Purpose
+The purpose of this code is to demonstate the seperation of concern at a functional level, following the single responsibilty principle.
+Each function only has a single purpose, and is all tied together in the game loop.
